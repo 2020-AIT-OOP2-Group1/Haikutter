d94a0ac6eb8900eab0c84233bea21fdc8b3282ba
Hisamatsu12 <k19087kk@aitech.ac.jp>
2020-12-13 23:41:04 +0900

    おかしなとこ教えてください

---
+++ static/main.js
@@ -0,0 +1,14 @@
+
+document.getElementById("send").addEventListener('submit', (e) => {
+  // ボタンイベントのキャンセル
+  e.preventDefault()
+  const obj = {text: document.getElementById("text").value, name: document.getElementById("name").value};
+  const method = "post";
+  const body = JSON.stringify(obj);
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  };
+fetch("./haiku", {method, headers, body}).then((res)=> res.json()).then(console.log).catch(console.error);
+});
+
