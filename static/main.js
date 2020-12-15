5ca540f65404b668eaac2a5ac93312e092799178
k19057 <75057552+k19057@users.noreply.github.com>
2020-12-15 23:32:54 +0900

    favorite機能実装
    favorite機能実装しました

--- static/main.js
+++ static/main.js
@@ -12,3 +12,18 @@ document.getElementById("send").addEventListener('click', (e) => {
 fetch("./haiku", {method, headers, body}).then((res)=> res.json()).then(console.log).catch(console.error);
 });
 
+
+// メモ : add => fav/ add-submit => favorite / fn => favorite
+document.getElementById("favorite").addEventListener("click", (e) => {
+  // ボタンイベントのキャンセル
+  e.preventDefault()
+
+  const obj = { "id": "fs7UBJYcxOCSN5oSa6zI6MwxUoOknyAB", "date": "2020-12-15-11:11:04", "text": "aaaaaaaaaaaa\n", "name": "aaaaaaa", "favorite": 0 };
+  const method = "post";
+  const body = JSON.stringify(obj);
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  };
+fetch("./haiku/favorite", {method, headers, body}).then((res)=> res.json()).then(console.log).catch(console.error);
+})
