6e82a85c71b631fda176abea07481b3651847d71
s-subway <75057402+s-subway@users.noreply.github.com>
2020-12-23 12:37:49 +0900

    login機能追加しました
    login機能追加しました

--- static/login.js
+++ static/login.js
@@ -3,13 +3,25 @@ document.getElementById("login").addEventListener("click", (e) => {
     // ボタンイベントのキャンセル
     e.preventDefault()
   
-    const obj = { "id": "hogehoge", "password": "PASSWORD"};
+    const obj = {
+       "user_id": document.getElementById("user-id").value,
+       "password": document.getElementById("password").value
+      };
+    console.log(obj)
     const method = "post";
     const body = JSON.stringify(obj);
+    console.log(body)
     const headers = {
-      'Accept': 'application/json',
+      'Accept': 'text/html',
       'Content-Type': 'application/json'
     };
-  fetch("./haiku/user/login", {method, headers, body}).then((res)=> res.json()).then(console.log).catch(console.error);
-  })
-  
+  fetch("./user/login", {method, headers, body})
+        .then((res)=> res.json())
+        .then((res)=> {
+          const d1 = document.getElementById("error-alert");
+          if (res["message"] == "Error"){
+            d1.style.display = "block";
+          }
+        })
+        .catch(console.error);
+});
