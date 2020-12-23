1dbb77a869ebd0514885960fe939b180500149db
harrki <k19016kk@aitech.ac.jp>
2020-12-24 00:12:24 +0900

    Merge branch 'main' into add-login-method
    修正しました

--- static/login.js
+++ static/login.js
@@ -2,26 +2,29 @@
 document.getElementById("login").addEventListener("click", (e) => {
     // ボタンイベントのキャンセル
     e.preventDefault()
-  
+
     const obj = {
-       "user_id": document.getElementById("user-id").value,
-       "password": document.getElementById("password").value
-      };
+        "user_id": document.getElementById("user-id").value,
+        "password": document.getElementById("password").value
+    };
     console.log(obj)
     const method = "post";
     const body = JSON.stringify(obj);
     console.log(body)
     const headers = {
-      'Accept': 'text/html',
-      'Content-Type': 'application/json'
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
     };
-  fetch("./user/login", {method, headers, body})
-        .then((res)=> res.json())
-        .then((res)=> {
-          const d1 = document.getElementById("error-alert");
-          if (res["message"] == "Error"){
-            d1.style.display = "block";
-          }
+    fetch("./user/login", { method, headers, body })
+        .then((res) => res.json())
+        .then((res) => {
+            const d1 = document.getElementById("error-alert");
+            if (res["message"] == "Error") {
+                d1.style.display = "block";
+            }
+            if (res["message"] == "Success") {
+                window.location.href = './';
+            }
         })
         .catch(console.error);
 });
